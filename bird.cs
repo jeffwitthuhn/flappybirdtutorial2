f4d408c4144d8bf93dac6c4b468543ca709cf506
jeffwitthuhn <jeffdwitt@gmail.com>
2017-03-21 18:14:31 -0500

    Add files via upload

---
+++ bird.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+    Rigidbody2D birdRB;
+    public float upspeed = 0.001f;
+    public float forewardspeed = 0.001f;
+    Vector3 up = new Vector3(0, 1, 0);
+    Vector3 right = new Vector3(1, 0, 0);
+    Vector3 zero = new Vector3(0, 0, 0);
+    bool start;
+    bool dead;
+	// Use this for initialization
+	void Start () {
+        start = false;
+        dead = false;
+        birdRB = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!dead)
+        {
+            if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Space))
+            {
+                birdRB.velocity = up * upspeed + right * forewardspeed;
+                if (!start)
+                {
+                    birdRB.gravityScale = 0.7f;
+                    start = true;
+                }
+            }
+        }
+        else
+        {
+            birdRB.velocity = zero;
+        }
+        
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collide with " + collision.gameObject.name);
+        if (collision.gameObject.name == "floor"
+            || collision.gameObject.name == "pipe")
+        {
+            birdRB.velocity = zero;
+            dead = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("triggered " + collision.gameObject.name);
+        if (collision.gameObject.name == "background" 
+            || collision.gameObject.name == "floor"
+            || collision.gameObject.name == "pipes")
+        {
+            Vector3 pos = collision.gameObject.transform.position;
+            
+            if (collision.gameObject.name == "pipes" || collision.gameObject.name == "floor" )
+            {
+                if(collision.gameObject.name == "pipes")
+                {
+                    float y = Random.Range(-0.7f, 0.7f);
+                    pos.y = y;
+                }
+                pos.x += 4 * 1.674f; 
+            }
+            else
+            {
+                pos.x += 4 * 1.42f;
+            }
+
+            collision.gameObject.transform.position = pos;
+
+        }
+
+
+    }
+}
---
+++ cameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+    Vector3 pos;
+    public GameObject player; //drag player onto this field in inspector
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+        pos = player.transform.position;
+        pos.z = -10;
+        pos.y = gameObject.transform.position.y;
+        gameObject.transform.position = pos;
+	}
+}
